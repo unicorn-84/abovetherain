1b61c772c48714646c2caac6d84a04a37d175fe7
unicorn-84 <unicorn-84@ya.ru>
2018-07-30 18:57:55 +0300

    Modify schedule styles.

--- src/pages/schedule/schedule.js
+++ src/pages/schedule/schedule.js
@@ -1,3 +1,8 @@
+import abovetherain from '../../scripts/init';
 import './schedule.scss';
 import '../../scripts/components/modal/modal';
 import '../../scripts/components/modal/modal.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  abovetherain.initialize();
+});
