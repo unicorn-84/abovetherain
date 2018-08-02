429a4f6f706e8c7bfce74dc1be35b0e3713b688e
unicorn-84 <unicorn-84@ya.ru>
2018-08-03 00:46:11 +0300

    Изменены стили для 'Мы'.

--- src/pages/team/team.js
+++ src/pages/team/team.js
@@ -1,4 +1,4 @@
 import 'reset-css';
-import '../../styles/scss/grid/flexboxgrid.scss';
+// import '../../styles/scss/grid/flexboxgrid.scss';
 import '../../styles/style.scss';
 import './team.scss';
