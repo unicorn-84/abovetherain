b051eb01461fd18833634525a3cd76e6a0a9177e
unicorn-84 <unicorn-84@ya.ru>
2018-08-08 10:30:59 +0300

    Добавлена initialize() на все страницы.

--- src/pages/events/events.js
+++ src/pages/events/events.js
@@ -3,4 +3,9 @@ import 'bootstrap/scss/bootstrap.scss';
 import '../../styles/style.scss';
 import './events.scss';
 // JS
+import abovetherain from '../../scripts/init';
 import '../../scripts/components/modal/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+  abovetherain.initialize();
+});
--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -3,4 +3,9 @@ import 'bootstrap/scss/bootstrap.scss';
 import '../../styles/style.scss';
 import './gallery.scss';
 // JS
+import abovetherain from '../../scripts/init';
 import '../../scripts/components/modal/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+  abovetherain.initialize();
+});
