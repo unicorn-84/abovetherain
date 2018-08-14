2abd42ea89432f9fac8d6cd2ae171fa5842d0eb9
unicorn-84 <unicorn-84@ya.ru>
2018-08-14 17:33:29 +0300

    Изменения на яндекс-карте.

--- src/pages/contacts/contacts.js
+++ src/pages/contacts/contacts.js
@@ -5,9 +5,6 @@ import './contacts.scss';
 // JS
 import abovetherain from '../../scripts/init';
 import '../../scripts/components/modal/modal';
-// require('../../images/logo/logo.png');
-
-import '../../images/mark.png';
 
 document.addEventListener('DOMContentLoaded', () => {
   abovetherain.initialize();
