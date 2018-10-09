c1424f6dc314a8b2c38f1932f8dadaa767ac14b0
unicorn-84 <unicorn-84@ya.ru>
2018-10-09 21:14:56 +0300

    Включена Афиша

--- dist/team.d55f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{25:function(n,e,t){"use strict";t(0),t(1),t(26);var i=function(n){return n&&n.__esModule?n:{default:n}}(t(2));t(3),document.addEventListener("DOMContentLoaded",function(){i.default.initialize()})},26:function(n,e,t){}},[[25,2,0,1]]]);
