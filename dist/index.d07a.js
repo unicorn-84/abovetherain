8a9e1f9e313ce1fb56e86812907dda3e26ad8fb9
unicorn-84 <unicorn-84@ya.ru>
2018-10-26 00:55:03 +0300

    1.2.0

---
+++ dist/index.d07a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{7:function(n,i,t){"use strict";t.r(i);t(1),t(2),t(8);var e=t(0);t(3);document.addEventListener("DOMContentLoaded",function(){e.a.initialize()})},8:function(n,i,t){}},[[7,1,2,0]]]);
