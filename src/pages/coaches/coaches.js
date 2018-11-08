7e8ef6060727edd2e9931d6eb79af72bb7b107fb
unicorn-84 <unicorn-84@ya.ru>
2018-11-08 15:24:19 +0300

    Удалены неиспользуемые скрипты

--- src/pages/coaches/coaches.js
+++ src/pages/coaches/coaches.js
@@ -1,13 +1,3 @@
-// JS
-import abovetherain from '../../scripts/init';
-import '../../scripts/components/modal/modal';
-// import createSelect from '../../scripts/components/select/select';
-// SCSS
 import 'bootstrap/scss/bootstrap.scss';
 import '../../styles/style.scss';
 import './coaches.scss';
-
-document.addEventListener('DOMContentLoaded', () => {
-  abovetherain.initialize();
-  // createSelect();
-});
