ea30a219c389874926d5c454e39a0e746c42e7ae
unicorn-84 <unicorn-84@ya.ru>
2018-11-24 00:31:49 +0300

    24.11.18

---
+++ src/scripts/components/modal/modal.js
@@ -0,0 +1,47 @@
+import './modal.scss';
+
+function createModalSpinner() {
+  const modalSpinner = document.createElement('div');
+
+  modalSpinner.classList.add('abovetherain__css3-spinner');
+  modalSpinner.innerHTML = '<div class="abovetherain__css3-spinner-bounce1"></div><div class="abovetherain__css3-spinner-bounce2"></div><div class="abovetherain__css3-spinner-bounce3"></div>';
+  return modalSpinner;
+}
+
+function createModalClose() {
+  const modalClose = document.createElement('div');
+
+  modalClose.classList.add('modal-close');
+  return modalClose;
+}
+
+function createModalOverlay() {
+  const modalOverlay = document.createElement('div');
+
+  modalOverlay.classList.add('modal-overlay');
+  modalOverlay.append(createModalSpinner());
+  return modalOverlay;
+}
+
+function createModal() {
+  const modal = document.createElement('div');
+
+  modal.classList.add('modal');
+  modal.append(createModalOverlay());
+  modal.append(createModalClose());
+  return modal;
+}
+
+function removeModal() {
+  document.body.classList.toggle('modal-active');
+  document.querySelector('.modal').remove();
+}
+
+function modalHandler() {
+  document.body.classList.toggle('modal-active');
+  document.body.prepend(createModal());
+  document.querySelector('.modal-overlay').addEventListener('click', removeModal);
+  document.querySelector('.modal-close').addEventListener('click', removeModal);
+}
+
+export default modalHandler;
