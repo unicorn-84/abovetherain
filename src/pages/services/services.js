62614ebeac0f001247431270b2e4ee3809a7cc6c
unicorn-84 <unicorn-84@ya.ru>
2018-12-28 17:19:03 +0300

    Изменен services.js

--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -1,40 +1,8 @@
 // Vendor
 // TODO: 'Добавить алиас'
 import 'bootstrap.scss';
-import 'carousel';
 // Common
 import '../../styles/style.scss';
 // Local
 import './services.scss';
 // Inline
-
-// import { fetchData, insertDataToModal } from '../../scripts/components/helpers';
-
-// document.body.addEventListener('click', (e) => {
-//   const target = e.target.closest('[data-ajax]');
-//   if (target) {
-//     import('../../scripts/components/modal/modal').then((module) => {
-//       module.modalHandler();
-//       fetchData(target.href, (error, htmlString) => {
-//         if (error) {
-//           console.log(error);
-//           return;
-//         }
-//         insertDataToModal(htmlString);
-//         if (document.querySelector('.carousel')) {
-//           $('.carousel').carousel();
-//         }
-//       });
-//     });
-//     e.stopPropagation();
-//     e.preventDefault();
-//   }
-// });
-
-// function insertData(data) {
-//   console.log('insert');
-//   document.querySelector('.modal').insertAdjacentHTML('beforeEnd', data);
-//   // document.querySelector('.modal').innerHTML = data;
-//   document.querySelector('.abovetherain__css3-spinner').remove();
-// }
-//
