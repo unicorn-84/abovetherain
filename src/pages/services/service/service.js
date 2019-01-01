7af7cd3f4b5b191c81caa24a5ffaf0c2979eb5d0
unicorn-84 <unicorn-84@ya.ru>
2019-01-02 01:25:25 +0300

    Добавлен alias для bootstrap collapse.js

--- src/pages/services/service/service.js
+++ src/pages/services/service/service.js
@@ -2,6 +2,7 @@
 // TODO: 'Добавить dynamic import Bootstrap carousel.js для rent.html'
 import 'bootstrap.scss';
 import 'carousel';
+import 'collapse';
 // Common
 import 'style.scss';
 import '../../../scripts/components/sticky';
