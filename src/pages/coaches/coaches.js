7aaae99411c9e4db62af6d486b7f392e65da3703
unicorn-84 <unicorn-84@ya.ru>
2019-01-16 17:53:49 +0300

    Добавлены coaches icons

--- src/pages/coaches/coaches.js
+++ src/pages/coaches/coaches.js
@@ -5,6 +5,12 @@ import 'collapse';
 import 'style.scss';
 import '../../scripts/components/sticky';
 import '../../scripts/components/social';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faInfo } from '@fortawesome/free-solid-svg-icons/faInfo';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt';
 // Local
 import './coaches.scss';
 // Inline
+
+library.add(faInfo, faCalendarAlt);
+dom.i2svg();
