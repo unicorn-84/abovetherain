8f7b851a3af4ca4705478a9ad89a1f64a4b23d3b
unicorn-84 <unicorn-84@ya.ru>
2019-01-31 23:37:44 +0300

    Изменен компонент 'helpers.js'

--- src/scripts/components/helpers.js
+++ src/scripts/components/helpers.js
@@ -1,10 +1,7 @@
-function wrap(element, wrapper) {
-  element.parentNode.insertBefore(wrapper, element);
-  wrapper.appendChild(element);
-}
-
-function retinaCheck() {
+function checkRetina() {
   return matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx)').matches;
 }
 
-export { wrap, retinaCheck };
+const mobile = matchMedia('(max-width: 991px)');
+
+export { checkRetina, mobile };
