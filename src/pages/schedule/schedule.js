3be198c3fad3d1ec342627106bd6df7b11a9fc1f
unicorn-84 <unicorn-84@ya.ru>
2019-01-31 14:20:28 +0300

    Изменен импорт 'underscore.js' в 'schedule.js'

--- src/pages/schedule/schedule.js
+++ src/pages/schedule/schedule.js
@@ -1,13 +1,9 @@
-// TODO: 'Удалить генератор'
-// TODO: 'Удалить custom select'
-// TODO: 'Добавить ссылки с фильтрацией по направлению'
-// TODO: 'Исправить смещение массива directions'
 // TODO: 'Генерировать стили'
 // Vendors
 import 'bootstrap.scss';
 import 'collapse';
 // Common
-import _ from 'underscore';
+import { each, has } from 'underscore';
 import '../../styles/main.scss';
 
 import '../../scripts/components/icons';
@@ -19,8 +15,8 @@ import { directions, coaches } from '../../data';
 import './schedule.scss';
 // Inline
 
+// Массив дней недели. Задается в скрипте.
 const days = [
-  'all',
   'monday',
   'tuesday',
   'wednesday',
@@ -29,27 +25,32 @@ const days = [
   'saturday',
   'sunday',
 ];
+// Объект для хранения массивов с названиями дней, направлений и тренеров у которых есть расписание.
 const data = {};
+// Объекту 'data' задается три ключа: 'direction', 'coach' и 'day'. Значение каждого ключа - массив строк. В каждый массив первым значением (index = 0) вставляется строка 'all', для фильтрации по 'Все дни', 'Все направления' и 'Все тренеры'. 'Глобальные' объекты 'directions' и 'coaches' из 'data.json' фильтруются по ключу 'schedule', чтобы в 'data' не попали направления и тренеры у которых нет расписания. У объекта у которого есть ключ 'schedule' берется значение ключа 'name' и оно 'пушится' соответственно либо в 'data.direction', либо в 'data.coach'.
 (function makeData() {
   data.direction = ['all'];
-  _.each(directions, (direction) => {
-    if (_.has(direction, 'schedule')) {
+  each(directions, (direction) => {
+    if (has(direction, 'schedule')) {
       data.direction.push(direction.name);
     }
   });
   data.coach = ['all'];
-  _.each(coaches, (coach) => {
-    if (_.has(coach, 'schedule')) {
+  each(coaches, (coach) => {
+    if (has(coach, 'schedule')) {
       data.coach.push(coach.name);
     }
   });
-  data.day = days;
+  data.day = ['all'];
+  data.day = data.day.concat(days);
 }());
+// Дефолтный 'stateObject'.
 const stateObject = {
   direction: 'all',
   coach: 'all',
   day: 'all',
 };
+// Создать строку запроса из объекта.
 function makeQuery(object) {
   return Object.keys(object).map(i => [i, object[i]].join('=')).join('&');
 }
@@ -94,11 +95,12 @@ function checkQuery() {
   }
   return false;
 }
+
 function replaceQuery(object) {
   window.history.replaceState(object, '', `/schedule.html?${makeQuery(makeStateObject(object))}`);
 }
-function setSelects(...selects) {
-  _.each(selects, (item) => {
+function setSelects(selects) {
+  each(selects, (item) => {
     const select = item;
     for (let i = 0; i < select.options.length; i += 1) {
       if (select.options[i].value === stateObject[select.id.split('-')[0]]) {
@@ -109,7 +111,7 @@ function setSelects(...selects) {
   });
 }
 function sortTable(tables) {
-  tables.forEach((table) => {
+  each(tables, (table) => {
     const item = table;
     if (stateObject.direction !== 'all') {
       item.dataset.selected = stateObject.direction;
@@ -118,34 +120,53 @@ function sortTable(tables) {
     }
   });
 }
+// Reset всех селектов в 'selectedIndex = 0', т.е. в 'Все тренеры', 'Все нправления' или 'Все дни' соответственно, кроме активного. Т.е. на котором был вызван 'onchange'.
+function closeAllSelect(target, selects) {
+  each(selects, (item) => {
+    const select = item;
+    if (target !== select) {
+      select.selectedIndex = 0;
+    }
+  }, selects);
+}
 document.addEventListener('DOMContentLoaded', () => {
   const tables = document.querySelectorAll('.schedule-table');
   const selects = document.querySelectorAll('.schedule-filter select');
-  const directionSelect = document.querySelector('select#direction-select');
-  const coachSelect = document.querySelector('select#coach-select');
-  const daySelect = document.querySelector('select#day-select');
-  _.each(selects, (item) => {
-    const select = item;
-    select.onchange = (e) => {
-      _.each(tables, (i) => {
-        const table = i;
-        table.dataset.selected = e.target.options[e.target.options.selectedIndex].getAttribute('value');
-      });
-      const object = {
-        direction: 'all',
-        coach: 'all',
-        day: 'all',
-      };
-      object[select.id.split('-')[0]] = data[select.id.split('-')[0]][select.options.selectedIndex];
-      replaceQuery(object);
-    };
-  });
+  // Проверка строки запроса
   if (checkQuery()) {
+    // При загрузке страницы всегда присваивать параметру 'day' значение 'all', т.к. запрос с фильтрацией по дням не используется.
     stateObject.day = 'all';
+    // Переписать строку запроса.
     replaceQuery(stateObject);
-    setSelects(directionSelect, coachSelect, daySelect);
+    // Програмно задать селектам значения в соответствии с значениями в строке запроса.
+    setSelects(selects);
+    // Присвоить 'value' выбранного селекта атрибуту 'data-select' у всех таблиц расписания.
     sortTable(tables);
+    // Назначение обработчика на 'onchange' всем селектам для фильтрации расписания.
+    each(selects, (item) => {
+      const select = item;
+      select.onchange = ({ target }) => {
+        // Присвоить 'value' выбранного селекта атрибуту 'data-select' у всех таблиц расписания
+        each(tables, (i) => {
+          const table = i;
+          table.dataset.selected = target.options[target.options.selectedIndex].getAttribute('value');
+        });
+        // Дефолтный 'stateObject' для обнудения строки запроса
+        const object = {
+          direction: 'all',
+          coach: 'all',
+          day: 'all',
+        };
+        // Определить 'id' выбранного селекта. Взять первую его часть. Найти в объекте 'data' по 'id' селекта и 'selectedIndex' строчное значение. Присвоить это значение соответствующему ключу в 'object'.
+        object[select.id.split('-')[0]] = data[select.id.split('-')[0]][select.options.selectedIndex];
+        // Переписать строку запроса
+        replaceQuery(object);
+        // Сбросить все селекты, кроме выбранного
+        closeAllSelect(target, selects);
+      };
+    });
   } else {
+    // Если пришел запрос с невалидными параметрами, задать дефолтные значения.
     replaceQuery(stateObject);
   }
 });
