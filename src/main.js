8dd786c29f561c3324585f8d6a3d89affda07fcd
unicorn-84 <unicorn-84@ya.ru>
2019-04-16 16:09:43 +0300

    Add events

--- src/main.js
+++ src/main.js
@@ -3,9 +3,52 @@ import './sass/main.scss';
 // JS
 import 'bootstrap';
 import createSchedule from './scripts/schedule';
+import events from './database/events';
+
+function createContent(name, day, start, end, coach) {
+  return `${name}`;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('#abovetherain__schedule')) {
+    const options = {
+      breakpoint: '991px',
+      container: 'abovetherain__schedule-table',
+      table: {
+        attributes: {
+          class: 'table table-sm table-bordered table-dark border border-rhino schedule-table text-center',
+        },
+        thead: {
+          td: {
+            attributes: {
+              class: 'font-weight-bold align-middle',
+            },
+          },
+        },
+        tbody: {
+          td: {
+            attributes: {
+              class: 'font-weight-bold align-middle',
+            },
+          },
+        },
+      },
+      rows: {
+        sort: true,
+      },
+      disableEmptyTable: true,
+      disableEmptyMobileTable: true,
+      disableEmptyMobileRow: true,
+      disableFirstMobileColumn: true,
+    };
+    options.events = events.map((x, i) => (
+      {
+        column: events[i].day,
+        row: events[i].start,
+        content: createContent(events[i].name, events[i].day, events[i].start, events[i].end, events[i].coach),
+      }
+    ));
+    window.scheduler(options);
     createSchedule();
   }
 });
