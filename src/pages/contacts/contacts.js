b035aa887d0a455ac47204af463aa1c867c9b70f
unicorn-84 <unicorn-84@ya.ru>
2019-04-17 14:43:26 +0300

    Add gallery

--- src/pages/contacts/contacts.js
+++
@@ -1,9 +0,0 @@
-// Local
-import './contacts.scss';
-// IMAGES
-import logo from '../../images/logo/logo.png';
-import evacuatorLogo from '../../images/icons/evacuator.png';
-
-(function createMap() {
-
-}());
